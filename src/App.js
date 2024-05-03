51630b056dc516859f5e7c0fece88d9caee916a2
Aman Chandrakar <aman.chandrakar@benisontech.com>
2024-05-03 17:02:33 +0530

    Ui created partially

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useCallback, useState } from 'react';
 
 function App() {
+  const[length,setLength] = useState(8);
+  const[numberAllowed,setNumberAllowed] = useState(false);
+  const[charAllowed,setCharAllowed] = useState(false);
+  const[password,setPassword] = useState("")
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+="
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  },[length,numberAllowed,charAllowed,setPassword])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8
+    text-orange-500 bg-gray-700 text-center'>
+      <h1 className='text-white text-2xl text-center my-3'>Password generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden my-6 mb-96'>
+        <input
+        type='text'
+        value={password}
+        placeholder='Password'
+        readOnly
+        className='outline-none w-full py-1 px-3'
+
+        />
+        <button className='outline-none bg-blue-600 text-white
+        px-3 py-0.5 shrink-0'>Copy</button>
+      </div>
     </div>
   );
 }
